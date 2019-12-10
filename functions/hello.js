610b454afbbc927c7d2ae12edf0a8478af963290
Ragu Sivanandha <ragu.sivanandha@gmail.com>
2019-12-10 11:43:33 -0500

    removed folder, updated command

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,35 @@
+const puppeteer = require("puppeteer");
+const request = require("request");
+
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || "World";
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}!`
-  };
+  const LINK = "";
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.emulateMedia("screen");
+
+    const response = await page.goto(LINK, {
+      waitUntil: "domcontentloaded"
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          status: response._status,
+          url: response._url,
+          statusText: response._statusText
+        },
+        null,
+        2
+      )
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 200,
+      body: `Failed: ${err}`
+    };
+  }
 };
