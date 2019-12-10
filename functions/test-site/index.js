2bdb427965990a38a3f5577ea1573fd9a41e0158
Ragu Sivanandha <ragu.sivanandha@gmail.com>
2019-12-10 15:47:54 -0500

    testing

--- functions/test-site/index.js
+++ functions/test-site/index.js
@@ -1,29 +1,33 @@
-const puppeteer = require("puppeteer");
-const request = require("request");
+// const puppeteer = require("puppeteer");
+// const request = require("request");
 
 exports.handler = async event => {
   const LINK = "";
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
 
-    await page.emulateMedia("screen");
+    // await page.emulateMedia("screen");
 
-    const response = await page.goto(LINK, {
-      waitUntil: "domcontentloaded"
-    });
+    // const response = await page.goto(LINK, {
+    //   waitUntil: "domcontentloaded"
+    // });
 
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify(
+    //     {
+    //       status: response._status,
+    //       url: response._url,
+    //       statusText: response._statusText
+    //     },
+    //     null,
+    //     2
+    //   )
+    // };
     return {
       statusCode: 200,
-      body: JSON.stringify(
-        {
-          status: response._status,
-          url: response._url,
-          statusText: response._statusText
-        },
-        null,
-        2
-      )
+      body: "all ok"
     };
   } catch (err) {
     console.log(err);
